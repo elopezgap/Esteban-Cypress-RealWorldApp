a357c3ce21dd32d3a36bf447fa8ccaf35835eae9
elopezgap <elopez@growthaccelerationpartners.com>
2023-06-23 16:53:06 -0600

    Assignment4 with extra elements

--- Esteban_assignment4.cy.js
+++ Esteban_assignment4.cy.js
@@ -1,5 +1,3 @@
-import { response } from "express";
-
 const apiBankAccounts = `${Cypress.env("apiUrl")}/bankAccounts`;
 
 describe('API Bank Account tests ', function () {
@@ -10,6 +8,7 @@ describe('API Bank Account tests ', function () {
         cy.login('Katharina_Bernier', 's3cret',{rememberUser:true});
     })
 
+    //Create Bank Accounts
     it('should create Bank Accounts', () => {
         for(let i=0; i < 5; i++){
             cy.request("POST",`${apiBankAccounts}`,{
@@ -21,6 +20,42 @@ describe('API Bank Account tests ', function () {
             })
         }
     })
+
+
+    //Check Bank Accounts
+    it('should list all banks', () => {
+        bankAccountID.forEach((value) => {
+            cy.request("GET", `${apiBankAccounts}/${value}`, {   
+            }).then((response) => {
+                expect(response.status).to.eq(200);
+                console.log(response)
+            });
+        })
+        cy.visit("/bankaccounts");
+    })
+
+
+    //Delete Bank Accounts
+    it('should delete all bank accounts', () => {
+        for(let i=0; i < 5; i++){
+            cy.request("POST",`${apiBankAccounts}`,{
+                "bankName": "APIBankTest"+i,
+                "accountNumber": "123456789"+i,
+                "routingNumber": "123123123"+i
+            }).then((response) => {bankAccountID.push(response.body.account.id);
+            expect(response.status).to.eq(200);
+            })
+        }
+        bankAccountID.forEach((value) => {
+            cy.request("DELETE", `${apiBankAccounts}/${value}`, {   
+            }).then((response) => {
+                expect(response.status).to.eq(200);
+                console.log(response)
+            });
+        })
+        cy.visit("/bankaccounts");
+    })
+
 
     
 })
